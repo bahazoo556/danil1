1cc781b20d98f1a01d88b4b54378a4f0f896a3cb
danil1 <zubahadanilka@gmail.com>
2024-04-17 16:45:51 +0300

    danil_server

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from config import TOKEN
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+   await bot.send_message(msg.chat.id, msg.text)
+
+
+if __name__ == '__main__':
+   executor.start_polling(dp)
